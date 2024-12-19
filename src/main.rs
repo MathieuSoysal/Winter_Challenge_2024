36d0f7c6bdfeb1555960ce04c66b8203278aeb96
Mathieu Soysal <hsoysal@student.42.fr>
2024-12-19 16:05:08 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+pub mod fib;
+use fib::fibonacci;
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let n = args
+        .get(1)
+        .expect("Give one argument")
+        .parse::<u32>()
+        .expect("Given argument should be a integer.");
+
+    println!("{}", fibonacci(n));
+}
