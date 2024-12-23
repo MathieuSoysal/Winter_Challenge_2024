e5eab8113790bdb7c877a3eec3556f86be83945d
Mathieu Soysal <hsoysal@student.42.fr>
2024-12-23 13:01:48 +0000

    test: add failed test for rogant root coord

--- src/game_entities/organ.rs
+++ src/game_entities/organ.rs
@@ -1,15 +1,24 @@
 use super::{coord::Coord, organ_direction::OrganDirection, organ_type::OrganType};
 
-const MASK_PLAYER: u16 = 0b0000_0001;
-const MASK_ORGAN_TYPE: u16 = 0b0001_1110;
-const MASK_ORGAN_DIRECTION: u16 = 0b1110_0000;
+const MASK_PLAYER: Organ = 0b0000_0001;
+const MASK_ORGAN_TYPE: Organ = 0b0001_1110;
+const MASK_ORGAN_DIRECTION: Organ = 0b1110_0000;
+const MASK_ROOT_ID: Organ = 0xFF00;
 
-pub type Organ = u16;
+pub type Organ = u32;
 
 pub fn new(owner: u8, organ_type: OrganType, organ_direction: OrganDirection) -> Organ {
-    (owner as u16 & MASK_PLAYER)
-        | ((organ_type as u16) << 1 & MASK_ORGAN_TYPE)
-        | ((organ_direction as u16) << 5 & MASK_ORGAN_DIRECTION)
+    (owner as Organ & MASK_PLAYER)
+        | ((organ_type as Organ) << 1 & MASK_ORGAN_TYPE)
+        | ((organ_direction as Organ) << 5 & MASK_ORGAN_DIRECTION)
+}
+
+pub fn add_root_coord(organ: Organ, root_coord: Coord) -> Organ {
+    organ | (((root_coord as u32) << 8) & MASK_ROOT_ID)
+}
+
+pub fn get_root_coord(organ: Organ) -> Coord {
+    ((organ & MASK_ROOT_ID) >> 8) as Coord
 }
 
 pub fn get_owner(organ: Organ) -> u8 {
@@ -50,4 +59,67 @@ pub fn is_tentacle(organ: Organ) -> bool {
 
 pub fn is_basic(organ: Organ) -> bool {
     get_type(organ) == OrganType::Basic
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::game_entities::coord;
+
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        let organ = new(0, OrganType::Root, OrganDirection::North);
+        assert_eq!(get_owner(organ), 0);
+        assert_eq!(get_type(organ), OrganType::Root);
+        assert_eq!(get_direction(organ), OrganDirection::North);
+    }
+
+    #[test]
+    fn test_add_root_coord() {
+        let organ = new(1, OrganType::Tentacle, OrganDirection::East);
+        let organ = add_root_coord(organ, 0xFFFF);
+        assert_eq!(get_owner(organ), 1);
+        assert_eq!(get_type(organ), OrganType::Tentacle);
+        assert_eq!(get_direction(organ), OrganDirection::East);
+        assert_eq!(0xFFFF, (organ & MASK_ROOT_ID) >> 8);
+    }
+
+    #[test]
+    fn test_get_owner() {
+        let organ = new(0, OrganType::Root, OrganDirection::North);
+        assert_eq!(get_owner(organ), 0);
+    }
+
+    #[test]
+    fn test_get_direction() {
+        let organ = new(0, OrganType::Root, OrganDirection::North);
+        assert_eq!(get_direction(organ), OrganDirection::North);
+    }
+
+    #[test]
+    fn test_get_type() {
+        let organ = new(0, OrganType::Root, OrganDirection::North);
+        assert_eq!(get_type(organ), OrganType::Root);
+    }
+
+    #[test]
+    fn test_is_faced_to() {
+        let organ = new(0, OrganType::Root, OrganDirection::North);
+        let organ_coord = coord::new(0, 1);
+        let coord = coord::new(0, 0);
+        assert!(is_faced_to(organ, organ_coord, coord));
+    }
+
+    #[test]
+    fn test_is_root() {
+        let organ = new(0, OrganType::Root, OrganDirection::North);
+        assert!(is_root(organ));
+    }
+
+    #[test]
+    fn test_is_harvester() {
+        let organ = new(0, OrganType::Harvester, OrganDirection::North);
+        assert!(is_harvester(organ));
+    }
 }
